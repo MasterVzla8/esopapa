96f2fccec66f5dc83239b0319e60c4265ad41fba
Jhonathan Otamendi <jhonathanguerrero030@gmail.com>
2024-06-23 10:50:11 -0400

    version 1.2

--- eso papa/main.cpp
+++ eso papa/main.cpp
@@ -4,13 +4,23 @@
 #include <iostream>
 using namespace std;
 
+int division(int valor, int divisor, int& resto) {
+	resto = valor % divisor;
+	return valor / divisor;
+}
+
 int main() {
-
-	int numero = 10;												//variable
-	cout << "valor de numero = " << numero << endl;					//imprimir la variable
-	cout << "ubicacion del numero = " << &numero << endl;			// imprimir ubicacion en memoria
-	cout << endl;
-
+	int numero;
+	int divisor;
+	int resto;
+	int resultado;
+	cout << "ingresa un numero ";
+	cin >> numero;
+	cout << "ingresa el divisor ";
+	cin >> divisor;
+	resultado = division(numero, divisor, resto);
+	cout << "el resultado es = " << resultado << endl;
+	cout << "el resto es = " << resto << endl;
 
 	cin.get();
 	return 0;
