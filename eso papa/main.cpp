65a761608ca8be2802d10c5a4eb08d2fb0e05ebb
Jhonathan Otamendi <jhonathanguerrero030@gmail.com>
2024-06-23 10:27:54 -0400

    version 1.1

--- eso papa/main.cpp
+++ eso papa/main.cpp
@@ -11,16 +11,6 @@ int main() {
 	cout << "ubicacion del numero = " << &numero << endl;			// imprimir ubicacion en memoria
 	cout << endl;
 
-	int* p_numero = &numero;										//almacenar ubicacion de memoria
-	
-	cout << "valor del punto = " << p_numero << endl;				//imprime la ubicacion de numero
-	cout << "ubicacion del punto = " << &p_numero << endl;			//imprime la ubicacion del puntero
-	cout << "valor de la ubi del numero = " << *p_numero << endl;	//imprime el numero de la variable
-	cout << endl;
-
-	numero = 20;
-	cout << "valor de la ubi del numero = " << *p_numero << endl;	//imprime el numero de la variable
-
 
 	cin.get();
 	return 0;
