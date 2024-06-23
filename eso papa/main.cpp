79f838df8bebd4f8f3acf732d93e20c640d814c5
Jhonathan Otamendi <jhonathanguerrero030@gmail.com>
2024-06-23 10:10:21 -0400

    versions 1.1 - sin CIN

--- eso papa/main.cpp
+++ eso papa/main.cpp
@@ -1,12 +1,12 @@
 //qlq dice esa plantilla para el flojoooo
+//que qqqqueeeee
 
 #include <iostream>
 using namespace std;
 
 int main() {
 
-	cout << "eso papa" << endl;
-	cin.get();
+	
 
 	return 0;
 }
